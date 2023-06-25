7a68e3552413e568dc6617d533938a4983cc62ad
maver <kentucky@posteo.de>
2023-06-25 18:20:53 +0200

    Add basic claude support

--- public/script.js
+++ public/script.js
@@ -89,7 +89,7 @@ import {
     is_get_status_openai,
     openai_messages_count,
     getTokenCountOpenAI,
-    chat_completion_sources,
+    chat_completion_sources, setupClaudePromptManager,
 } from "./scripts/openai.js";
 
 import {
@@ -4017,6 +4017,18 @@ function changeMainAPI() {
         getStatus();
         getHordeModels();
     }
+
+    console.log(oai_settings.chat_completion_source)
+    switch (oai_settings.chat_completion_source) {
+        case chat_completion_sources.OPENAI:
+            console.log('Setting up OpenAI prompt manager');
+            setupOpenAIPromptManager(oai_settings);
+            break;
+        case chat_completion_sources.CLAUDE:
+            console.log('Setting up Claude prompt manager');
+            setupClaudePromptManager(oai_settings);
+            break;
+    }
 }
 
 ////////////////////////////////////////////////////
@@ -4593,13 +4605,13 @@ async function getSettings(type) {
 
         // OpenAI
         loadOpenAISettings(data, settings);
-                setupOpenAIPromptManager(settings);
 
         // Horde
         loadHordeSettings(settings);
 
         // Poe
         loadPoeSettings(settings);
+        // setupPoePromptManager(settings);
 
         // Load power user settings
         loadPowerUserSettings(settings, data);
